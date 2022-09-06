62360948fff266a8550d2f11faeee34d12794f2a
Santiago Zappa <santiagozappab@gmail.com>
2022-09-06 16:33:06 -0500

    vercel fix 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
+    "extends": "plugin:react/recommended",
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
