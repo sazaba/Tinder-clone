560d5f2e2914dee5d154bf9f4da5a88a37ea15d9
Santiago Zappa <santiagozappab@gmail.com>
2022-09-06 11:36:24 -0500

    final project

---
+++ Components/SidebarRow.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  title: string;
+  onClick?: () => {};
+}
+
+function SidebarRow({ Icon, title, onClick }: Props) {
+  return (
+    <div
+      onClick={() => onClick?.()}
+      className="group flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200"
+    >
+      <Icon className="h-5 -w-5" />
+      <p className="hidden md:inline-flex text-base font-light lg:text-xl group-hover:text-twitter">
+        {title}
+      </p>
+    </div>
+  );
+}
+
+export default SidebarRow;
---
+++ Components/TweetBox.tsx
@@ -0,0 +1,134 @@
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+import { Tweet, TweetBody } from "../typing";
+import { fetchTweets } from "../utils/fetchTweets";
+import toast from "react-hot-toast";
+
+interface Props {
+  setTweets: Dispatch<SetStateAction<Tweet[]>>;
+}
+
+function TweetBox({ setTweets }: Props) {
+  const [image, setImage] = useState<string>("");
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const [input, setInput] = useState<string>("");
+  const { data: session } = useSession();
+  const [imageUrBoxlIsOpen, setImageUrBoxlIsOpen] = useState<boolean>(false);
+  session?.user;
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (!imageInputRef.current?.value) return;
+    setImage(imageInputRef.current.value);
+    imageInputRef.current.value = "";
+    setImageUrBoxlIsOpen(false);
+  };
+
+  const postTweet = async () => {
+    const tweetInfo: TweetBody = {
+      text: input,
+      username: session?.user?.name || "Unknown User",
+      profileImg:
+        session?.user?.image ||
+        "https://i.postimg.cc/26101XfQ/kisspng-computer-icons-google-account-user-profile-iconfin-png-icons-download-profile-5ab0301e32cb90.png",
+      image: image,
+    };
+    const result = await fetch(`/api/addTweet`, {
+      body: JSON.stringify(tweetInfo),
+      method: "POST",
+    });
+    const json = await result.json();
+    const newTweets = await fetchTweets();
+    setTweets(newTweets);
+
+    toast("Tweet Posted");
+    return json;
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    postTweet();
+    setInput("");
+    setImage("");
+    setImageUrBoxlIsOpen(false);
+  };
+
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        className="h-14 w-14 object-cover rounded-full mt-4"
+        src={
+          session?.user?.image ||
+          "https://i.postimg.cc/NjPJc8T0/default-user-image-png-4-Transparent-Images.png"
+        }
+      />
+
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+            type="text"
+            placeholder="What's happening ?"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <div className="flex items-center">
+            <div className="flex space-x-2 text-twitter flex-1">
+              <PhotographIcon
+                onClick={() => {
+                  setImageUrBoxlIsOpen(!imageUrBoxlIsOpen);
+                }}
+                className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+              />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
+            </div>
+            <button
+              onClick={handleSubmit}
+              disabled={!input || !session}
+              className="bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+          {imageUrBoxlIsOpen && (
+            <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+              <input
+                ref={imageInputRef}
+                className="flex-1  bg-transparent p-2 text-white outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter Image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+          {image && (
+            <img
+              className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+              src={image}
+            />
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TweetBox;
